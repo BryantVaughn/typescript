68b2d53db07a744e5a2f3a58d35bce83bdc176fc
BryantVaughn <bryant.f.vaughn@gmail.com>
2021-01-16 14:58:26 -0700

    Add comments for detail

--- types/types.ts
+++ types/types.ts
@@ -1,6 +1,9 @@
+// today holds a value of type Date
 const today = new Date();
 today.getMonth();
 
+// age: number
+// person: { age: number }
 const person = {
   age: 20
 };
@@ -9,4 +12,5 @@ class Color {
 
 }
 
+// red: Color
 const red = new Color();
