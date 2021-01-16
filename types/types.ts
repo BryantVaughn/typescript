fea5213e04f2cb6618d58de160f7aa7547cf3193
BryantVaughn <bryant.f.vaughn@gmail.com>
2021-01-16 15:49:43 -0700

    Annotation examples for different variable types

--- types/types.ts
+++ types/types.ts
@@ -14,3 +14,12 @@ class Color {
 
 // red: Color
 const red = new Color();
+
+/**
+ * Type Annotations & Type Inference
+ */
+
+// Type annotations - Code we add to tell Typescript what type of value a variable will refer to (We tell Typescript the type)
+// Type inference   - Typescript tries to figure out what type of value a variable refers to (Typescript guesses the type)
+
+
