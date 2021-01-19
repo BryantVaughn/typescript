93a6320909e99cd696382b7d4ba16ae02b0ea498
BryantVaughn <bryant.f.vaughn@gmail.com>
2021-01-19 11:51:55 -0700

    Constructor inheritance and super

--- types/classes.ts
+++ types/classes.ts
@@ -1,10 +1,20 @@
 class Vehicle {
+  constructor(public color: string) {}
+
   protected honk(): void {
     console.log('beep');
   }
 }
 
+const vehicle = new Vehicle('blue');
+console.log(vehicle.color);
+
+
 class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
+
   private drive(): void {
     console.log('vroom');
   }
@@ -15,7 +25,7 @@ class Car extends Vehicle {
   }
 }
 
-const car = new Car();
+const car = new Car(4, 'orange');
 car.startDrivingProcess();
 
 
